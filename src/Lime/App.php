7492c97bda924d8c0fd5fb016dda65b4ae099932
Timothy <timgblack@hotmail.com>
2019-02-02 17:43:39 +0800

    Default App::instance to get LimeApp

--- src/Lime/App.php
+++ src/Lime/App.php
@@ -261,7 +261,7 @@ class App implements \ArrayAccess {
     * @param  String $name Lime app name
     * @return Object       Lime app object
     */
-    public static function instance($name) {
+    public static function instance($name = 'LimeApp') {
         return self::$apps[$name];
     }
 
